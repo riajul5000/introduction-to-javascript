b13bd74ab892a676a6e10792c31337fe69d83859
riajul5000 <riyazulislam500@gmail.com>
2023-07-29 10:04:17 +0200

    some tags added

---
+++ math.js
@@ -0,0 +1,16 @@
+/***
+ * Math needed to start larning a programming language 
+ * 1. number ==> 55,898, -32
+ * 2. integer , add, even: 2,5,
+ * 3. float: decimal: 2,5 13.67,3269
+ * 4. operation: addition,substraction,multiplication,
+ * 5,percent
+ * 6. average
+ * 7. comprasion:>,<,==,>=, =<
+ * 8.oikik
+ * 9.45,25,68,129,2: max,min,average,sum
+ * 10, series
+ * 11. prime
+ * 12.permutation
+ * 13. area
+ */
