82f5f81c203d20b09db7b65da03c00971a35e1c9
riajul5000 <riyazulislam500@gmail.com>
2023-07-29 12:09:48 +0200

    4file and added some javascript tags

---
+++ variable-name.js
@@ -0,0 +1,33 @@
+//good variable
+var price = 29;
+
+//.1 variable name can not be any keywords
+var false =  96;
+var return = true;
+
+// 2. variable name has to be in one wrk. No Space 
+ var my home address ="canada";
+
+ //3. variable name can not have quotation
+ var "name"= "tom hanks";
+
+//4. variable name can not starts with a number but can ends with a number
+var 99Club = 1964;
+var club25 = 2025;
+
+
+//5. How to use long names
+// cannot use dash
+var user=name ="taj";
+ 
+
+var user_name ="bappa raj";
+var usercurrenthomeaddress = "andor killa bandor bon";
+var user_home_address = "andor killa bandor bon"; //snake case
+var userHomeAddress = "andor killa bandor bon"; //camel case : we will use this one
+var UserHomeAddress = "andor killa bandor bon" //pascal case
+
+//variable name is case sensitive
+var person = 25;
+var Person = 35;
+
---
+++ variable.js
@@ -0,0 +1,15 @@
+ //numeric
+ var price = 13;
+ var age =17;
+ var temperature = 38;
+ var height = 10;
+ var flor = 4;
+ var size= 3;
+//string
+ var person = "riajul islam";
+ var location = "milan italy";
+ var special = "Olsen"
+//boolean
+ var serious =true;
+ var isFullMarks = true;
+ var isSingle = false;
